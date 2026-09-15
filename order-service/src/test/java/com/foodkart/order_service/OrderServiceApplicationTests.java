8be11fc30c33c965c953df326d2763a7786a9546
AmitNegi24 <mramitnegig1999@gmail.com>
2026-09-16 02:58:45 +0530

    Added Service skeleton

---
+++ order-service/src/test/java/com/foodkart/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodkart.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
