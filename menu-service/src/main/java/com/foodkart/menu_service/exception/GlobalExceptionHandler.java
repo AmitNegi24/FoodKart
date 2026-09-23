063c4a6b5514d4b1ad04ce3058a9a18523f03689
AmitNegi24 <mramitnegig1999@gmail.com>
2026-09-23 23:37:26 +0530

    removed unused imports

--- menu-service/src/main/java/com/foodkart/menu_service/exception/GlobalExceptionHandler.java
+++ menu-service/src/main/java/com/foodkart/menu_service/exception/GlobalExceptionHandler.java
@@ -8,10 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import java.time.LocalDateTime;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
